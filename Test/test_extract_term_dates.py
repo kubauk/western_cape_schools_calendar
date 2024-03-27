6e72640f0d8c1d51ea260bb3647171e3adc32ffe
Kuba Jamro <kuba.jamro@gmail.com>
2024-03-27 18:07:51 +0200

    adding missing types

--- Test/test_extract_term_dates.py
+++ Test/test_extract_term_dates.py
@@ -4,10 +4,10 @@ import sys
 import pytest
 import bs4
 
-A_VALID_YEAR = "2026"
+A_VALID_YEAR: str = "2026"
 
 
-def is_valid_calender_date(possible_date):
+def is_valid_calender_date(possible_date: str) -> bool:
     try:
         the_date = get_calender_date(possible_date, A_VALID_YEAR)
     except ValueError:
@@ -15,18 +15,18 @@ def is_valid_calender_date(possible_date):
     return the_date is not None
 
 
-def get_calender_date(date_string, year):
+def get_calender_date(date_string: str, year: str) -> datetime.datetime:
     return datetime.datetime.strptime("{} {}".format(date_string, year), "%d %B %Y")
 
 
 @pytest.mark.parametrize("value, expected_result",
                          [("1 March", True), ("1", False), ("1 March 2023", False), ("31 June", False),
                           ("30 June", True), ("First", False)])
-def test_is_date(value, expected_result):
+def test_is_date(value: str, expected_result: str) -> None:
     assert is_valid_calender_date(value) == expected_result
 
 
-def extract_dates_from_table(soup, year):
+def extract_dates_from_table(soup, year: str) -> None:
     text = soup.get_text(separator='\n', strip=True).splitlines()
     list_of_text_to_tuple_of_dates(text, year)
 
@@ -56,13 +56,13 @@ def test_list_of_text_to_tuple_of_dates(lines, dates):
     assert list_of_text_to_tuple_of_dates(lines, "2026") == dates
 
 
-def test_extract_all_dates_from_sample_table():
+def test_extract_all_dates_from_sample_table() -> None:
     with open("2024 School Calendar:.html", "r") as input:
         soup = bs4.BeautifulSoup(input)
         extract_dates_from_table(soup, "2026")
 
 
-def test_extract_this_years_dates():
+def test_extract_this_years_dates() -> None:
     with open("School Calendar and Public Holidays _ Western Cape Education Department.html", "r") as input:
         soup = bs4.BeautifulSoup(input)
         headers = soup.find_all("h5")
